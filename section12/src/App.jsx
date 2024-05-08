7643d1d9a66515bdde50632f643f5fbeac786225
bccha <morer@naver.com>
2024-05-09 01:30:43 +0900

    84강 까지

--- section12/src/App.jsx
+++ section12/src/App.jsx
@@ -4,12 +4,7 @@ import Home from "./pages/Home";
 import New from "./pages/New";
 import Diary from "./pages/Diary";
 import Notfound from "./pages/Notfound";
-
-import emotion1 from "./assets/emotion1.png";
-import emotion2 from "./assets/emotion2.png";
-import emotion3 from "./assets/emotion3.png";
-import emotion4 from "./assets/emotion4.png";
-import emotion5 from "./assets/emotion5.png";
+import { getEmotionImage } from "./util/get-emotion-images";
 
 // 1. '/' 모든 일기 조회 Home
 // 2. '/new' 새로운 일기 작성
@@ -20,11 +15,11 @@ function App() {
   return (
     <>
       <div>
-        <img src={emotion1} />
-        <img src={emotion2} />
-        <img src={emotion3} />
-        <img src={emotion4} />
-        <img src={emotion5} />
+        <img src={getEmotionImage("1")} />
+        <img src={getEmotionImage("2")} />
+        <img src={getEmotionImage("3")} />
+        <img src={getEmotionImage("4")} />
+        <img src={getEmotionImage("5")} />
       </div>
       <div>
         <Link to={"/"}>Home</Link>
